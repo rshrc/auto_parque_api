6e892b7b96b5626684cb8894b7c923507f3c1315
Rishi Banerjee <rishieric91@gmail.com>
2019-08-29 22:58:34 +0530

    Working Server

---
+++ zone/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from zone.models import Zone, Index, Slot
+
+admin.site.register(Zone)
+admin.site.register(Index)
+admin.site.register(Slot)
---
+++ zone/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Zone(models.Model):
+    pass
+
+
+class Index(models.Model):
+    pass
+
+
+class Slot(models.Model):
+    pass
