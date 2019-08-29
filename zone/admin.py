6e9d645a73a1cf338be8e19ea63495fc18bb6c2e
Rishi Banerjee <rishieric91@gmail.com>
2019-08-29 23:26:46 +0530

    :wrench: improved and upgraded admin

--- zone/admin.py
+++ zone/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from zone.models import Zone, Index, Slot
 
-admin.site.register(Zone)
-admin.site.register(Index)
-admin.site.register(Slot)
+admin.site.site_header = 'Parque Space'
+
+
+@admin.register(Zone)
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Index)
+class IndexAdmin(admin.ModelAdmin):
+    list_display = ['number']
+
+
+@admin.register(Slot)
+class SlotAdmin(admin.ModelAdmin):
+    list_display = ['zone', 'index', 'empty']
--- zone/models.py
+++ zone/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 
 class Zone(models.Model):
-    pass
+    name = models.CharField(max_length=3, default="")
+
+    def __str__(self):
+        return self.name
 
 
 class Index(models.Model):
-    pass
+    number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.number)
 
 
 class Slot(models.Model):
-    pass
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE, default="")
+    index = models.ForeignKey(Index, on_delete=models.CASCADE, default="")
+    empty = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.zone.name) + " " + str(self.index.number) + " " + str(self.empty)
