a965ac4c73ced59a7530f390b115dea0399ac789
Rishi Banerjee <rishieric91@gmail.com>
2019-09-15 15:53:03 +0530

    code refactor

--- entry_log/models.py
+++ entry_log/models.py
@@ -2,6 +2,7 @@ import uuid
 from datetime import datetime
 from django.db import models
 
+
 class EntryLog(models.Model):
     car_id = models.UUIDField(default=uuid.uuid4, editable=True, unique=True)
     entry_time = models.DateTimeField(auto_now=False, auto_now_add=False, default=datetime.now())
--- entry_log/views.py
+++ entry_log/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from rest_framework import generics
 from entry_log.models import EntryLog
 from entry_log.serializers import EntryLogSerializer
