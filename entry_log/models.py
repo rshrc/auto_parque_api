22a8fe7c9eb71e8ade4a06ec3ccf4f7484dfb364
Rishi Banerjee <rishieric91@gmail.com>
2019-09-16 22:47:26 +0530

    writing exception with status codes

--- entry_log/models.py
+++ entry_log/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class EntryLog(models.Model):
-    car_id = models.UUIDField(default=uuid.uuid4, editable=True, unique=True)
+    car_id = models.CharField(max_length=100, default=0)
     entry_time = models.DateTimeField(auto_now=False, auto_now_add=False, default=datetime.now())
 
     def __str__(self):
